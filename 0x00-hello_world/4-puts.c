999bdcfa6165e3278f8ecea144db94a62d3749cf
Lawrence <lawrencesipopo@gmail.com>
2023-02-16 13:35:12 -0800

    Hello, puts again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 
 /**
- * main we going to print \"Programming is like building a multilingual puzzle
- * return 0
+ * main - Prints "Programming is like building a multilingual
+ * 		   puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
