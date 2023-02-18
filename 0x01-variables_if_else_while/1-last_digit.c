4e6523ce387a9b5f4cd7d3fb20fa0fc8568ea0e5
Lawrence <lawrencesipopo@gmail.com>
2023-02-17 17:50:29 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
 
 /**
  * main - Prints the last digit of a randomly generated nunber
- * 	and whether it is greater than 5, less than 6, or 0.
+ *	and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
