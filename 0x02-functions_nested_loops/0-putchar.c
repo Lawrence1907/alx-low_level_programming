af5b2a3f9e3fe1f60a82da8aa00ef230bef871b8
Lawrence <lawrencesipopo@gmail.com>
2023-02-21 12:37:31 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ * file-name: 0-putchar.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+
+	return (0);
+}
+
