da53fb8042a5a35706ed3af9743d0abeace7f362
Lawrence <lawrencesipopo@gmail.com>
2023-02-22 12:22:28 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints a number that is above, equal and nagative.
+ *
+ * @n: Input numbers as an integer.
+ * Return: 1 is greater than zero.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('43');
+
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+
+		return (0);
+	}
+	else 
+		_putchar('45');
+
+	_putchar('\n');
+}
