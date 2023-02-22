db269cd44059030ea19a98028fdc9b8cfa164e8e
Lawrence <lawrencesipopo@gmail.com>
2023-02-21 19:52:19 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,14 +12,15 @@
 
 int _putchar(char p)
 {
-	_putchar('_')';
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	_putchar('h')
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-i        print(	return (0);
+	
+	return (0);
 }
 
