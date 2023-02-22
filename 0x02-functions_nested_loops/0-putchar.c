dbb1983c06df25e9f110ee367bdb101ec3799b2c
Lawrence <lawrencesipopo@gmail.com>
2023-02-21 20:00:06 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,18 +10,8 @@
  * Return: if successful it returns 1.
  */
 
-int _putchar(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
 
