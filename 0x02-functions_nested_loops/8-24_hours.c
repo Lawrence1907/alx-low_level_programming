2af87a931ca7de528d5040dc43a950472225b501
Lawrence <lawrencesipopo@gmail.com>
2023-02-22 16:19:06 -0800

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer, 
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59.
  */
 
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			_putchar((a / 10) + '0');
 			_putchar((a % 10) + '0');
 			_putchar(':');
-			_putchar((i / 10 ) + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar('\n');
 		}
