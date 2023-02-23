1c895a05bbcc65f65d6619cb5295a0c39d8566d0
Lawrence <lawrencesipopo@gmail.com>
2023-02-22 16:33:44 -0800

    prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,8 @@
  * print_to_98 - print all natural numbers from n to 98.
  * @n: the number to start counting from to 98.
  *
- * Return: void return.
+ * Return: void return
+ */.
 
 
 void print_to_98(int n)
