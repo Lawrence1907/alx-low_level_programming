082d18c5d86c57a37ab7c9e6bdddef59decd3e3b
Lawrence <lawrencesipopo@gmail.com>
2023-02-23 15:48:49 -0800

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,11 +16,17 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size; j++)
-				_putchar(' ');
-
-			for (j = 1; j <= i; j++)
-				_putchar(35);
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size -i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(35);
+				}
+			}
 			_putchar('\n');
 		}
 	}
