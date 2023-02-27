daa36c9f61a73b65ab06bf060e539c36de93813a
Lawrence <lawrencesipopo@gmail.com>
2023-02-27 12:09:19 -0800

    Function that prints a string.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,15 @@
 
 
 /**
- * _puts - Write a function that prints a string, follewd by a new line to stdout.
- * @str this my input string.
+ * _puts - Write a function that prints a string, follewd by a new line
+ * to stdout.
+ * @str: this my input string.
  */
 void _puts(char *str)
 {
 	int i;
 
-	for (i =0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
