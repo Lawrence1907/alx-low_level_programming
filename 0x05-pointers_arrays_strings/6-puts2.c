78e3af23dc301b86431d883ab19c9563538696ea
Lawrence <lawrencesipopo@gmail.com>
2023-02-27 14:52:17 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+void puts2(char *str)
+{
+	int i = 0, x = 0, n;
+	char *y = str;
+
+	while (*y != '\0')
+	{
+		y++;
+		i++;
+	}
+
+	x = i - 1;
+
+	for (n = o; n <= x; n++)
+	{
+		if (o % 2 ==0)
+			_putchar(str[o]);
+	}
+	_putchar('\n');
+}
+
