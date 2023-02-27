7c22a86bb5affdeafa1b283b9a7100a102cccc10
Lawrence <lawrencesipopo@gmail.com>
2023-02-27 11:36:16 -0800

    Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
  * swap_int- Swaps two integers.
  * @a: the value of a is given to b.
  * @b: the value of b is given to a.
- * @c: this variable is a temporary holder.
  */
 
 void swap_int(int *a, int *b)
