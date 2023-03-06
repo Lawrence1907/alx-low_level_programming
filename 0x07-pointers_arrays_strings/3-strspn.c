59dd72785fb98992162af88c27ebc3461eebb14b
Lawrence <lawrencesipopo@gmail.com>
2023-03-06 13:30:54 -0800

    3. strspn - gets the length of a prefix substring.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,9 +3,9 @@
 /**
  * _strspn - gets the length of a prefix substring.
  * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in @s.
- *
- * Return: counter
+ * @accept: the bytes to include.
+ * Return: the number of bytes in the initial segment of @s which consist only
+ * bytes from @accept.
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 		if (s[i] != 32)
 		{
 			for (j = 0; accept[j] != '\0'; j++)
-			{			
+			{
 				if (s[i] == accept[j])
 				{
 					counter++;
