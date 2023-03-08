bd7cad35d66b4996c6503f507aeb0beb0a116a84
Lawrence <lawrencesipopo@gmail.com>
2023-03-08 09:22:35 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd>
+
+/**
+ * _putchar - writes character c to stdout.
+ * @c: character to print.
+ *
+ * Return: 1, On success.
+ * On Error, -1.
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
