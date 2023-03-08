1667edb2fff8bda115fb9a4dab658789570d9323
Lawrence <lawrencesipopo@gmail.com>
2023-03-08 13:22:37 -0800

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,36 @@
 #include "main.h"
 
 /**
- * palind - obtains length of a.
+ * palind2 - obtains length of a
  * @a: string
- * @l: integer to count length.
+ * @l: integer to count length
  *
- * Return: 1, On success.
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int palind(char *a)
+int palind2(char *a, int l)
 {
-	if (*a == '\0')
-		return (0);
-	else
-		return (1 + palind(a + 1));
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
 }
-
 /**
- * palind2 - compares string vs string reverse.
+ * palind3 - compares string vs string reverse
  * @a: string
- * @n1: smallest iterator.
- * @n2: biggest iterator.
+ * @l: length
  *
- * Return: 1, On success.
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int palind2(char *a, int n1, int n2)
+int palind3(char *a, int l)
 {
-	if (*(a + n1) == *(a + n2))
-	{
-		if (n1 == n2 || n2 + 1)
-			return (1);
-		return (0 + palind2(a, n1 + 1, n2 - 1));
-	}
-	return (0);
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
 }
-
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to evaluate
@@ -48,7 +41,8 @@ int palind2(char *a, int n1, int n2)
 
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (1);
-	return (palind2(s, 0, palind(s) - 1));
+	int l;
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
